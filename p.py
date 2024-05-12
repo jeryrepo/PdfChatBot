c79d59013076a3bb651018cb83f13973e671341c
jeryrepo <142509067+jeryrepo@users.noreply.github.com>
2024-05-12 23:17:29 +0500

    p.py

--- p.py
+++ p.py
@@ -12,7 +12,6 @@ import pyttsx3
 load_dotenv()
 
 inference_api_key="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"
-groq_api_key="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"
 
 def text_to_speech(text):
     engine = pyttsx3.init()
@@ -48,7 +47,7 @@ def get_conversational_chain():
     Answer:
     """
 
-    model = ChatGroq(temperature=0, model_name="llama3-8b-8192")
+    model = ChatGroq(temperature=0, groq_api_key="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", model_name="llama3-8b-8192")
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
     chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
